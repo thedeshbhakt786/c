8b5338a2c09430e559544e9fee7a90650929aca0
thedeshbhakt786 <72206760+thedeshbhakt786@users.noreply.github.com>
2020-10-01 21:45:42 +0530

    Update a.js

--- a.js
+++ a.js
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>To Do List</title>
+    <title>To Do List for Someone</title>
   </head>
   <body>
     <h1>It's <%=day%> </h1>
